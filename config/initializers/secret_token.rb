4d29e083e16c722115c8ff6affe305d41b435653
Jon Beer <bringmebeer@gmail.com>
2015-11-20 18:49:14 +0000

    Endpoint to receive and send on build status to the jelly tank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jbm::Application.config.secret_token = '9f165cf6ca8982e719a26f598601646b0189842e43da0a1007440f904a3fe42a310070c737795ed4b92e30e800abe72758ffdf6a9a603bad01a0c792bb21b539'
